c1b24a05851c7a19a639cdaa08dd96529d3ec10d
Angie <arco527@gmail.com>
2019-11-05 17:50:04 -0500

    Result success

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
-}
+export const App = () => {
+
+  const [char, setChar] = useState(0)
+
+  return (
+    <div className="container">
+      <textarea onChange={e => setChar(e.target.value.length)} rows="3"></textarea>
+      <div className="counter">{char}</div>
+    </div>
+  );
+
+};
 
 export default App;
